ed9166f4f964e3e31e65d2b1ce5b57e7f5bc1406
Fitzwilliam <darcy@lizzy>
2023-09-27 16:52:57 +0200

    DFS ok

--- tp0.py
+++ tp0.py
@@ -24,7 +24,7 @@ def build_graph(n: int, connec_factor: float) -> dict[int, list[int]]:
         result[i] = tmp
     return result
 
-graph = build_graph(5, 0.5)
+graph = build_graph(10, 0.3)
 
 def visualize_graph(datapoints: list[tuple[float]], graph: dict[int, list[int]]) -> None:
     for i in datapoints:
@@ -34,7 +34,7 @@ def visualize_graph(datapoints: list[tuple[float]], graph: dict[int, list[int]])
             plt.plot([datapoints[i][0], datapoints[j][0]], [datapoints[i][1], datapoints[j][1]], 'c-')
     plt.show()
 
-visualize_graph(sample_datapoints(5), graph)
+visualize_graph(sample_datapoints(10), graph)
 
 def shortest_path_length_bfs(graph: dict[int, list[int]], start: int, goal: int) -> int:
     visited = []
@@ -58,84 +58,17 @@ def shortest_path_length_bfs(graph: dict[int, list[int]], start: int, goal: int)
     print("Failure")
     return -1
 
-print(shortest_path_length_bfs(graph, 2, 4))
+print(shortest_path_length_bfs(graph, 2, 7))
 
-
-# def shortest_path_length_dfs(graph: dict[int, list[int]], start: int, goal: int) -> int:
-#     tab = []
-#     visited = []
-#     tab.append(start)
-#     while (len(tab) != 0):
-#         vertex = tab.pop(0)
-#         for i in graph[vertex]:
-#             if i not in visited:
-#                 visited.append(i)
-#                 if (i == goal):
-#                     print("Success")
-#                     return 0
-#                 tab.insert(0, i)
-#     print("Failure")
-#     return -1
-# shortest_path_length_dfs(graph, 2, 4)
-
-var = 0
-
-def dfs(graph: dict[int, list[int]], start: int, goal: int, visited: list[int], path: int) -> int:
-    if (start == goal):
-        print("Success: path_length=" + str(path))
-        return path
-    visited.append(start)
+def DLS(graph: dict[int, list[int]], start: int, goal: int, limit: int) -> bool:
+    if (start == goal): return True;
+    if (limit <= 0): return False;
     for i in graph[start]:
-        if (i not in visited):
-            return dfs(graph, i, goal, visited, path + 1)
+        if DLS(graph, i, goal, limit - 1): return True;
+    return False;
 
-print(dfs(graph, 2, 4, [], 0))
+def IDDFS(graph: dict[int, list[int]], start: int, goal: int, limit: int) -> int:
+    for i in range(limit):
+        if DLS(graph, start, goal, i): return i;
 
-def shortest_path_length_dfs(graph: dict[int, list[int]], start: int, goal: int) -> int:
-    return dfs(graph, start, goal, [], 0)
-
-print(shortest_path_length_dfs(graph, 2, 4))
-
-def dfs_2(graph: dict[int, list[int]], start: int, goal: int) -> list[int]:
-    if (start == goal):
-        return start
-    explored = []
-    tab = []
-    tab.append(start)
-    while (len(tab) != 0):
-        vertex = tab.pop(0)
-        explored.append(vertex)
-        for i in graph[vertex]:
-            if (i not in explored):
-                if i == goal: return i
-                tab.append(i)
-    return -1
-def dfs(graph, start, target, path = [], visited = set()):
-    path.append(start)
-    visited.add(start)
-    if start == target:
-        return path
-    for neighbour in graph[start]:
-        if neighbour not in visited:
-            result = dfs(graph, neighbour, target, path, visited)
-            if result is not None:
-                return result
-    path.pop()
-    return None
-        
-print(dfs(graph, 2, 4))
-# def shortest_path_length_bfs(graph: dict[int, list[int]], start: int, goal: int) -> int:
-#     tab = []
-#     visited = []
-#     tab.append(start)
-#     while (len(tab) != 0):
-#         vertex = tab.pop(0)
-#         for i in graph[vertex]:
-#             if i not in visited:
-#                 visited.append(i)
-#                 if (i == goal):
-#                     print("Succes")
-#                     return -1
-#                 tab.append(i)
-#     return -1
-# shortest_path_length_bfs(build_graph(5, 0.5), 2, 4)
+print(IDDFS(graph, 2, 7, 10))
