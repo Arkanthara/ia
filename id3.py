e53cc5a340313f03abfdb524e4bc21d5fe607b8f
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2023-11-29 17:10:24 +0100

    Backup

---
+++ id3.py
@@ -0,0 +1,29 @@
+import csv
+import numpy as np
+
+
+file = open('data.csv', 'r')
+#with open('data.csv', 'r') as file:
+
+A = []
+B = []
+C = []
+D = []
+c = []
+
+data = []
+
+reader = csv.reader(file)
+
+for row in reader:
+    data.append(row)
+
+data = np.array(data)
+
+print(data)
+
+entropy = lambda x:  -x * np.log2(x)
+
+print(data[1:,-1])
+
+print(entropy(np.sum(np.array([int(i) for i in data[1:,-1]])) / (len(data) - 1) ))
