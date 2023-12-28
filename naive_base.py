c8841fe50a3cce31856bc89d9f7aeb7cdf1b34d6
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2023-12-28 18:54:10 +0100

    Backup

--- naive_base.py
+++ naive_base.py
@@ -42,7 +42,7 @@ def distrib_param(data: np.ndarray, header: np.ndarray, target: int):
                     tmp_2["Mean"] = np.mean(newdata[:, j].astype(float))
                     tmp_2["Variance"] = np.var(newdata[:, j].astype(float))
                 tmp[header[j]] = tmp_2
-        result[values[i]] = tmp
+        result[values[i]] = (len(newdata) / len(data), tmp)
     return result
 
 
@@ -55,14 +55,20 @@ def naive_base(distrib: dict, header: np.ndarray, data: np.ndarray):
     for i in values_keys:
         denom = 1
         for j in range(len(header)):
-            if list(distrib[i][header[j]].keys())[0] == "Mean" or list(distrib[i][header[j]].keys())[1] == "Mean":
-                denom *= norm.cdf(float(data[j]), distrib[i][header[j]]["Mean"], distrib[i][header[j]]["Variance"])
+            if list(distrib[i][1][header[j]].keys())[0] == "Mean" or list(distrib[i][1][header[j]].keys())[1] == "Mean":
+                denom *= norm.cdf(float(data[j]), distrib[i][1][header[j]]["Mean"], distrib[i][1][header[j]]["Variance"])
             else:
-                denom *= distrib[i][header[j]][data[j]]
-        if denom > maximum:
+                denom *= distrib[i][1][header[j]][data[j]]
+        print(denom)
+
+        if denom * distrib[i][0] > maximum:
             maximum = denom
             value = i
-    return value
+
+    print(data)
+    print(maximum)
+    print(value)
+    return int(value)
 
 print(naive_base(distrib_param(data, header, 3), ["Gender", "Age"], ["Male", '35']))
 
@@ -72,5 +78,35 @@ print(data_test)
 
 FN, FP, TN, TP = range(4)
 
-def test_naive_base(distrib: dict, header: np.ndarray, data: np.ndarray):
-    if 
+def test_naive_base(distrib: dict, header: np.ndarray, data: np.ndarray, target: int):
+    target_values = data[:, target].astype(int)
+    data = np.delete(data, target, 1)
+    header = np.delete(header, target)
+
+    FN_count = 0
+    FP_count = 0
+    TN_count = 0
+    TP_count = 0
+    for i in range(len(data)):
+        result = naive_base(distrib, header, data[i])
+
+        if result == 1 and target_values[i] == 1:
+            TP_count += 1
+        elif result == 0 and target_values[i] == 0:
+            TN_count += 1
+        elif result == 1 and target_values[i] == 0:
+            FP_count += 1
+        elif result == 0 and target_values[i] == 1:
+            FN_count += 1
+        else:
+            print(type(result))
+            print(type(target_values[i]))
+    print(FN_count)
+    print(FP_count)
+    print(TN_count)
+    print(TP_count)
+    return FN_count / len(data), FP_count / len(data), TN_count / len(data), TP_count / len(data)
+
+
+print(test_naive_base(distrib_param(data, header, 3), header, data_test, 3))
+print(test_naive_base(distrib_param(data, header, 3), header, data, 3))
